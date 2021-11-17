42c1f2049811435e2d5f98d3ffc04bec2957ca25
esyede <suyadi.1992@gmail.com>
2021-11-18 00:41:38 +0700

    Console: forget to reset the database

--- system/console/boot.php
+++ system/console/boot.php
@@ -10,6 +10,8 @@ use System\Config;
 // Boot paket default agar seluruh dependensi terdaftar di Container.
 Package::boot(DEFAULT_PACKAGE);
 
+$default = Config::get('database.default');
+
 // Set database default jika user mengoper '--database'.
 if (! is_null($database = get_cli_option('database'))) {
     Config::set('database.default', $database);
@@ -21,9 +23,12 @@ require path('system').'console'.DS.'dependencies.php';
 // Bungkus error kedalam try-catch agar lebih mudah dibaca.
 try {
     Console::run(array_slice($arguments, 1));
+    Config::set('database.default', $default);
 } catch (\Throwable $e) {
+    Config::set('database.default', $default);
     echo $e->getMessage();
 } catch (\Exception $e) {
+    Config::set('database.default', $default);
     echo $e->getMessage();
 }
 
