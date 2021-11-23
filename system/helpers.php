5fcb64e834f811bfac4e61f96d7eb5a792d4ee17
esyede <suyadi.1992@gmail.com>
2021-11-23 15:35:11 +0700

    Helper: add new `human_filesize` helper

--- system/helpers.php
+++ system/helpers.php
@@ -665,4 +665,32 @@ if (! function_exists('system_os')) {
 
         return isset_or($platforms[PHP_OS], 'Unknown');
     }
+
+    if (! function_exists('human_filesize')) {
+        /**
+         * Format ukuran file (ramah manusia).
+         *
+         * @param int $size
+         * @param int $precision
+         *
+         * @return string
+         */
+        function human_filesize($size, $precision = 2)
+        {
+            $size = (int) $size;
+            $precision = (int) $precision;
+
+            $units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+            $i = 0;
+
+            while (($size / 1024) > 0.9) {
+                $size = $size / 1024;
+                $si++;
+            }
+
+            $size = ($size < 0) ? ($size + (2.0 * (PHP_INT_MAX + 1))) : $size;
+
+            return round($size, $precision).' '.$units[$i];
+        }
+    }
 }
