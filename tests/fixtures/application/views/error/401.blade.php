4a1c7365090700ccb1b097e15c999d6275d2a4d3
esyede <suyadi.1992@gmail.com>
2021-12-07 19:56:53 +0700

    View: simplify error templates

---
+++ tests/fixtures/application/views/error/401.blade.php
@@ -0,0 +1,29 @@
+<?php defined('DS') or exit('No direct script access.'); ?>
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="noindex">
+    <meta name="generator" content="Rakit debugger">
+    <link rel="icon" type="image/png" href="data:;base64,iVBORw0KGgo=">
+    <title>{{ $code }} | {{ $message }}</title>
+    <style>
+        #oops-error {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+            min-height: 95vh;
+            color: #a0aec0;
+            color: rgba(160, 174, 192, 1);
+            font-size: 1.125em;
+            text-transform: uppercase;
+        }
+    </style>
+</head>
+<body>
+    <div id="oops-error">
+        {{ $code }} | {{ $message }}
+    </div>
+</body>
+</html>
