4cd6988bd2ddcb7b37e688783904b17682fd6976
esyede <suyadi.1992@gmail.com>
2021-12-13 01:10:37 +0700

    Helper: fix human_filesize() faiiling on a large integers

--- system/helpers.php
+++ system/helpers.php
@@ -793,11 +793,12 @@ if (! function_exists('system_os')) {
          */
         function human_filesize($size, $precision = 2)
         {
+            $size = (float) $size;
             $units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
             $power = ($size > 0) ? floor(log($size, 1024)) : 0;
             $power = ($power > (count($units) - 1)) ? (count($units) - 1) : $power;
 
-            return number_format($size / pow(1024, $power), $precision, '.', ',').' '.$units[$power];
+            return sprintf('%s %s', round($size / pow(1024, $power), $precision), $units[$power]);
         }
     }
 }
