5ba5458044d0a22909e2b0650ca4b3a6f00ed9ca
Suyadi <suyadi.1992@gmail.com>
2022-03-10 00:17:49 +0700

    Str: simplify uuid generation

--- system/str.php
+++ system/str.php
@@ -478,15 +478,12 @@ class Str
      */
     public static function uuid()
     {
-        $uuid = bin2hex(static::bytes(16));
-        return sprintf(
-            '%08s-%04s-4%03s-%04x-%012s',
-            substr($uuid, 0, 8),
-            substr($uuid, 8, 4),
-            substr($uuid, 13, 3),
-            hexdec(substr($uuid, 16, 4)) & 0x3fff | 0x8000,
-            substr($uuid, 20, 12)
-        );
+        $bytes = static::bytes(16);
+
+        $bytes[6] = chr(ord($bytes[6]) & 0x0f | 0x40);
+        $bytes[8] = chr(ord($bytes[8]) & 0x3f | 0x80);
+
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($bytes), 4));
     }
 
     /**
