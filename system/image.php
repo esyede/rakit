2ecacf016205e0e625a54afb94e894ce5be41c61
Suyadi <suyadi.1992@gmail.com>
2022-04-02 19:05:09 +0700

    Image: add image type info

--- system/image.php
+++ system/image.php
@@ -522,6 +522,7 @@ class Image
 
         return [
             'path' => $this->path,
+            'type' => $type.
             'width' => $this->width,
             'height' => $this->height,
             'quality' => $this->quality,
