73b1da1acb73646475ec7f7e84fb8d3a379cb77e
esyede <suyadi.1992@gmail.com>
2022-10-30 14:04:51 +0700

    small changes

--- paths.php
+++ paths.php
@@ -100,6 +100,10 @@ if (PHP_VERSION_ID < 80100) {
     }
 }
 
+// --------------------------------------------------------------
+// Polyfill untuk Stringable interface.
+// -------------------------------------------------------------
+
 if (PHP_VERSION_ID < 80000) {
     interface Stringable
     {
