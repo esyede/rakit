ec7cc58efe44be13d08a8c8694540f66348fbe06
esyede <suyadi.1992@gmail.com>
2022-12-22 02:31:28 +0700

    Schema: fix sqlite fails on listing tables

--- system/database/schema.php
+++ system/database/schema.php
@@ -54,8 +54,10 @@ class Schema
                 break;
 
             case 'sqlite':
-                $query = 'SELECT table_name FROM sqlite_master' .
-                    " WHERE type = 'table' AND table_name NOT LIKE 'sqlite_%'";
+                $query = "SELECT name FROM sqlite_master " .
+                    "WHERE type IN ('table','view') AND name NOT LIKE 'sqlite_%' " .
+                    "UNION ALL SELECT name FROM sqlite_temp_master " .
+                    "WHERE type IN ('table','view') ORDER BY 1";
                 break;
 
             case 'sqlsrv':
