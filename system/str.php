237af65d098ad1369c86b0e00c0fdcba564d8258
esyede <suyadi.1992@gmail.com>
2023-03-03 19:33:08 +0700

    small changes

--- system/str.php
+++ system/str.php
@@ -331,31 +331,28 @@ class Str
      *
      * @return string
      */
-    public static function password($length = 32, $letters = true, $numbers = true, $symbols = true, $spaces = false)
-    {
-        $chars = [
-            'letters' => [
+    public static function password(
+        $length = 32,
+        $letters = true,
+        $numbers = true,
+        $symbols = true,
+        $spaces = false
+    ) {
+        $chars = array_merge(
+            [],
+            $letters ? [
                 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
                 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
                 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
                 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
                 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-            ],
-            'numbers' => ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
-            'symbols' => [
-                '~', '!', '#', '$', '%', '^', '&', '*', '(', ')', '-',
-                '_', '.', ',', '<', '>', '?', '/', '\\', '{', '}', '[',
-                ']', '|', ':', ';',
-            ],
-            'spaces' => [' '],
-        ];
-
-        $chars = array_merge(
-            [],
-            $letters ? $chars['letters'] : [],
-            $numbers ? $chars['numbers'] : [],
-            $symbols ? $chars['symbols'] : [],
-            $spaces ? $chars['spaces'] : []
+            ] : [],
+            $numbers ? ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] : [],
+            $symbols ? [
+                '~', '!', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '.',
+                ',', '<', '>', '?', '/', '\\', '{', '}', '[', ']', '|', ':', ';',
+            ] : [],
+            $spaces ? [' '] : []
         );
 
         $max = count($chars) - 1;
