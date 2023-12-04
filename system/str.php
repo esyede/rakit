33ba804e0743c3731f1aa121e139bb9079de7f09
esyede <suyadi.1992@gmail.com>
2023-12-04 07:36:56 +0700

    small changes

--- system/str.php
+++ system/str.php
@@ -609,22 +609,22 @@ class Str
         $pool = explode(
             ' ',
             'a ab ad accusamus adipisci alias aliquam amet animi aperiam ' .
-                'architecto asperiores aspernatur assumenda at atque aut beatae ' .
-                'blanditiis cillum commodi consequatur corporis corrupti culpa ' .
-                'cum cupiditate debitis delectus deleniti deserunt dicta ' .
-                'dignissimos distinctio dolor ducimus duis ea eaque earum eius ' .
-                'eligendi enim eos error esse est eum eveniet ex excepteur ' .
-                'exercitationem expedita explicabo facere facilis fugiat harum ' .
-                'hic id illum impedit in incidunt ipsa iste itaque iure iusto ' .
-                'laborum laudantium libero magnam maiores maxime minim minus ' .
-                'modi molestiae mollitia nam natus necessitatibus nemo neque ' .
-                'nesciunt nihil nisi nobis non nostrum nulla numquam occaecati ' .
-                'odio officia omnis optio pariatur perferendis perspiciatis ' .
-                'placeat porro possimus praesentium proident quae quia quibus ' .
-                'quo ratione recusandae reiciendis rem repellat reprehenderit ' .
-                'repudiandae rerum saepe sapiente sequi similique sint soluta ' .
-                'suscipit tempora tenetur totam ut ullam unde vel veniam vero ' .
-                'vitae voluptas'
+            'architecto asperiores aspernatur assumenda at atque aut beatae ' .
+            'blanditiis cillum commodi consequatur corporis corrupti culpa ' .
+            'cum cupiditate debitis delectus deleniti deserunt dicta ' .
+            'dignissimos distinctio dolor ducimus duis ea eaque earum eius ' .
+            'eligendi enim eos error esse est eum eveniet ex excepteur ' .
+            'exercitationem expedita explicabo facere facilis fugiat harum ' .
+            'hic id illum impedit in incidunt ipsa iste itaque iure iusto ' .
+            'laborum laudantium libero magnam maiores maxime minim minus ' .
+            'modi molestiae mollitia nam natus necessitatibus nemo neque ' .
+            'nesciunt nihil nisi nobis non nostrum nulla numquam occaecati ' .
+            'odio officia omnis optio pariatur perferendis perspiciatis ' .
+            'placeat porro possimus praesentium proident quae quia quibus ' .
+            'quo ratione recusandae reiciendis rem repellat reprehenderit ' .
+            'repudiandae rerum saepe sapiente sequi similique sint soluta ' .
+            'suscipit tempora tenetur totam ut ullam unde vel veniam vero ' .
+            'vitae voluptas'
         );
 
         $max = ($max <= 3) ? 4 : $max;
